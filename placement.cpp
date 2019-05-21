27f7bc480ec52ee390716d5410c8fab4e41952a2
barlette <leobarlette@gmail.com>
2019-05-21 01:32:23 -0300

    Implementado o posicionamento utilizando arvore de adjacencia. Ainda faltam alguns pequenos ajustes (e.g. espelhamento do transistor para que os nodos da net fiquem mais próximos).

--- placement.cpp
+++ placement.cpp
@@ -49,6 +49,49 @@ int find_nonvis_pair(int u, vector< pair<int, int> > edges, vector <int> vis, in
     return -1;
 }
 
+int count_vertex_degree(vector < vector<int> > adj, int n, int vert){
+    int sum = 0;
+    for(int column = 0; column < n; column++){
+        sum = sum+adj[vert][column];
+    }
+    return sum-2;
+}
+
+int find_leaf(vector < vector<int> > adj, int n, vector <int> visited){
+    //cout << "teste";
+    for(int vert = 0; vert < n; vert++){
+        if((count_vertex_degree(adj, n, vert) == 0) && (visited[vert] == 0)){
+            //cout << vert;
+            return vert;
+        }
+    } 
+    return -1;
+}
+
+int find_connected_vertice(vector < vector<int> > adj, int n, vector <int> visited){
+    for(int row = 0; row < adj.size(); row++){
+            if((adj[row][n] == 1) && (row != n) && (visited[row] == 0))
+                return row;
+    }
+    return -1;
+}
+
+int find_connected_vertice_toleaf(vector < vector<int> > adj, int n, vector <int> visited){
+    for(int column = 0; column < adj.size(); column++){
+            if((adj[n][column] == 1) && (column != n) && (visited[column] == 0))
+                return column;
+    }
+    return -1;
+}
+
+int find_nonvis_node(vector < vector<int> > adj, int n, vector <int> visited){
+    for(int column = 0; column < adj.size(); column++){
+        if((adj[n][column] == 1) && (column != n) && (visited[column] == 0))
+            return column;
+    }
+    return -1;
+}
+
 void place(string fileName){
     vector<Transistor> nSt;
     vector<Transistor> pSt;
@@ -74,9 +117,8 @@ void place(string fileName){
     }
     
     int maxFins = nFins + pFins + 4;
-    string grade[maxFins][(2*maxTracks)-1];
     int totalTracks = (2*maxTracks)-1;
-    //vector< vector<int> > grade;
+
     
     int estWidth = 54*maxTracks;
     int estHeight = 64 + 20*(maxFins-1) + 7*maxFins;
@@ -110,20 +152,25 @@ void place(string fileName){
     for(int it = 0; it < mst_wt.size(); it++)
         cout << mst_wt[it].first << " - " << mst_wt[it].second << "\n";
     
+    vector <int> v(netlist_p.size(), 0);
+    vector < vector<int> > adjMatrix(netlist_p.size(), v);
     
-    int adjMatrix[netlist_p.size()][netlist_p.size()] = {};
+//    for(int i=0; i < netlist_p.size(); i++){
+//        adjMatrix.push_back(v);
+//    }
     
     //monta matriz de adjacencia
     for(int row = 0; row < netlist_p.size(); row++){
         for(int column = 0; column < netlist_p.size(); column++){
             if(row == column)
                 adjMatrix[row][column]=1;
-            }
         }
-    
+    }
     
     for(int it = 0; it < mst_wt.size(); it++){
+        //cout << "first: " << mst_wt[it].first << "second: " << mst_wt[it].second << "\n";
         adjMatrix[mst_wt[it].first][mst_wt[it].second] = 1;
+        adjMatrix[mst_wt[it].second][mst_wt[it].first] = 1;
     }
     
     
@@ -135,15 +182,76 @@ void place(string fileName){
         cout << "\n";
         }
     
-    vector <int> pos[netlist_p.size()];
+    vector <int> pos(netlist_p.size(), -1);
+    vector <int> visited(netlist_p.size());
+
     
-    for(int row = 0; row < netlist_p.size(); row++){
-        for(int column = 0; column < netlist_p.size(); column++){
-            
+    int it=0;
+    
+    while(it < netlist_p.size()){
+    int vert = find_leaf(adjMatrix, netlist_p.size(), visited);
+    if(vert != -1){
+        pos[it] = vert;
+        cout << vert;
+        visited[pos[it]] = 1;            
+    } else cout << "Não achou vértice folha restante.\n";
+    vert = 0;
+    it++;
+    while(vert != -1){
+        
+        vert =  find_connected_vertice(adjMatrix, pos[it-1], visited);
+        if(vert != -1){
+            pos[it] = vert;
+            cout << pos[it];
+            it++;
+            visited[vert] =1;
+        }
+    }
+    //cout << pos[it-1];
+    vert=0;
+    if(count_vertex_degree(adjMatrix, netlist_p.size(), pos[it-1]) > 1){
+        vert = find_nonvis_node(adjMatrix, pos[it-1], visited);
+        pos[it] = vert;
+        cout << vert;
+        it++;
+        visited[vert]=1;
+    }
+    
+    while(vert != -1){    
+        //cout << pos[it-1];
+        vert =  find_connected_vertice_toleaf(adjMatrix, pos[it-1], visited);
+        if(vert != -1){
+            pos[it] = vert;
+            cout << pos[it];
+            it++;
+            visited[vert] =1;
+        }
+    }
+    }
+    
+    string grade[maxFins][(2*maxTracks)-1];
+    cout << "\n";
+    string lastName = " ";
+    int trackIndex = 0;
+    for(int it = 0; it < pos.size(); it++){
+        //cout << it;
+        //cout << lastName << netlist_p[it].name;
+        if(lastName != netlist_p[pos[it]].name){
+            for(int row=0; row<maxFins; row++){
+                grade[row][trackIndex] = netlist_p[pos[it]].name;    
             }
+            trackIndex++;
+            lastName = netlist_p[pos[it]].name;        
+        }
+    }
+
+    for(int row = 0; row < maxFins; row++){
+        for(int column = 0; column < trackIndex; column++){
+            cout << grade[row][column] << " ";
+        }
         cout << "\n";
-        }   
-    
+    }
+
 }
     
     
